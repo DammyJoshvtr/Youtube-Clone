9d072aef72eac9421beda1d43f019fa01a303fa1
Joshua Damilola <damilolajoshua2021@gmail.com>
2024-09-18 14:29:38 -0700

    Deleted and Included Some Thumbnails to the Page

--- scripts/data.js
+++ scripts/data.js
@@ -116,4 +116,144 @@ const videoGrid = [{
       }
     }
   }
-},]
+},
+
+{
+  videoPreview: {
+    thumbnailRow: {
+      thumbnailImage: 'thumbnails/thumbnail-7.webp',
+      videoTime: '29:37'
+    },
+
+    videoInfoGrid: {
+      channelPicture: {
+        profilePicture: 'authors_pic/channel-7.jpeg'
+      },
+      videoInfo: {
+        videoTitle: "How to put an HTML website online (on the Internet)",
+        videoAuthor: 'SuperSimpleDev',
+        videoStats: '1.6M views &#183; 2 years ago'
+      }
+    }
+  }
+},
+
+{
+  videoPreview: {
+    thumbnailRow: {
+      thumbnailImage: 'thumbnails/thumbnail-8.webp',
+      videoTime: '32:06'
+    },
+
+    videoInfoGrid: {
+      channelPicture: {
+        profilePicture: 'authors_pic/channel-8.jpg'
+      },
+      videoInfo: {
+        videoTitle: "FC 24 - Real Madrid Vs Manchester City - Ft. Mbappe| UEFA UCL 24/25...",
+        videoAuthor: 'FIFAGamerSS',
+        videoStats: '5.9K views &#183; 5 days ago'
+      }
+    }
+  }
+},
+
+{
+  videoPreview: {
+    thumbnailRow: {
+      thumbnailImage: 'thumbnails/thumbnail-9.webp',
+      videoTime: '28:07'
+    },
+
+    videoInfoGrid: {
+      channelPicture: {
+        profilePicture: 'authors_pic/channel-9.jpg'
+      },
+      videoInfo: {
+        videoTitle: "FC 24 - Barcelona Vs. Real Madrid Ft. Yamal, Mbappe, Williams, | La Liga...",
+        videoAuthor: 'Mzpanda Gaming',
+        videoStats: '121K views &#183; 2 months ago'
+      }
+    }
+  }
+},
+
+{
+  videoPreview: {
+    thumbnailRow: {
+      thumbnailImage: 'thumbnails/thumbnail-10.webp',
+      videoTime: '9:56:04'
+    },
+
+    videoInfoGrid: {
+      channelPicture: {
+        profilePicture: 'authors_pic/channel-10.jpg'
+      },
+      videoInfo: {
+        videoTitle: "Buld and Deploy 3 Modern UI/UX Websites and Get Hired as Front...",
+        videoAuthor: 'JavaScript Mastery',
+        videoStats: '324K views &#183; 3 months ago'
+      }
+    }
+  }
+},
+
+{
+  videoPreview: {
+    thumbnailRow: {
+      thumbnailImage: 'thumbnails/thumbnail-11.webp',
+      videoTime: '8:02'
+    },
+
+    videoInfoGrid: {
+      channelPicture: {
+        profilePicture: 'authors_pic/channel-12.jpg'
+      },
+      videoInfo: {
+        videoTitle: "How I MASTERED CODING with these 4 easy steps",
+        videoAuthor: 'Levi Okoye',
+        videoStats: '103K views &#183; 2 months ago'
+      }
+    }
+  }
+},
+
+{
+  videoPreview: {
+    thumbnailRow: {
+      thumbnailImage: 'thumbnails/thumbnail-12.webp',
+      videoTime: '5:01'
+    },
+
+    videoInfoGrid: {
+      channelPicture: {
+        profilePicture: 'authors_pic/channel-13.jpg'
+      },
+      videoInfo: {
+        videoTitle: '"Heart Attack"' +"(Demi Lovato) Cover from " + "'School of Rock' 🎶 | Music...",
+        videoAuthor: 'NickRewind',
+        videoStats: '651K views &#183; 1 year ago'
+      }
+    }
+  }
+},
+
+{
+  videoPreview: {
+    thumbnailRow: {
+      thumbnailImage: 'thumbnails/thumbnail-13.webp',
+      videoTime: '3:04:05'
+    },
+
+    videoInfoGrid: {
+      channelPicture: {
+        profilePicture: 'authors_pic/channel-14.jpg'
+      },
+      videoInfo: {
+        videoTitle: "Build and Deploy a Portfolio Website Using Next JS, TailWind CSS & Fram...",
+        videoAuthor: 'Cristian Mihai',
+        videoStats: '281K views &#183; 4 months ago'
+      }
+    }
+  }
+}]
