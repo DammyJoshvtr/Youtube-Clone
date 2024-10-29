edff5aba96df6a716f60a74a915da4e629db97d4
Joshua Damilola <damilolajoshua2021@gmail.com>
2024-10-28 20:55:17 -0700

    Designed the html elements for the video-playback with css

--- scripts/video-view.js
+++ scripts/video-view.js
@@ -2,26 +2,14 @@ import { videoGrid, playBackGrid } from "./data";
 
 let videoHTML = '';
 
-const html=`
-  <div class="video-view">
-    <img src="thumbnails/thumbnail-1.webp">
-  </div>
-  <div class="video_info_grid">
-    <div class="channel_picture">
-      <img src="authors_pic/channel-1.jpeg" class="profile_picture"> 
-    </div>
-    <div>
-      <p>Talking Tech and AI with the Google CEO Sunday Punchai!</p>
-    </div>
-  </div>
-`;
-
-playBackGrid.forEach((video) => {
-  videoHTML += html;
+playBackGrid.forEach((playback) => {
+  videoPlayBackHTML += `
+    
+  `;
 
 })
 
-console.log(videoHTML);
+console.log(videoPlayBackHTML);
 
 document.getElementById('video-view-container')
   .innerHTML = videoHTML;
